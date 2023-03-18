65080d1efa5fe62622d61c0df6b017bb2b7c6699
AMAL-GIL2001 <amal.gil1998@gmail.com>
2023-03-17 17:01:45 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * Main - prints the alphabet in lowercase, followed by a new lin except q and e
+ *
+ * Return : Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++){
+		
+		if(c != 'e' && c != 'q'){
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
