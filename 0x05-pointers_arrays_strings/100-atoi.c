3f8feb4ff477296e96eb2c44182a9aa5d6746cfd
AMAL-GIL2001 <amal.gil1998@gmail.com>
2023-03-28 18:36:02 -0700

    a function that converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,14 +13,21 @@ int _atoi(char *s)
 	int i;
 	int len = strlen(s);
 	int num;
+	int count = 0;
 
 	for (i = 0; i < len; i++)
 	{
 		if(s[i] >= '0' && s[i] <= '9')
-			num = s[i];
+		{
+			num = num * 10 + (s[i] + 48);
+                        count++;  
+		}
 		else
 	
 			continue;
 	}
+	if (count == 0)
+		return (0);
+	
 	return (num);
 }
