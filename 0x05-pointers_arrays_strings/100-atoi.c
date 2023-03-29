76a7928cf5411fd9fdf0e531b3555b0449eb852d
AMAL-GIL2001 <amal.gil1998@gmail.com>
2023-03-28 19:06:31 -0700

    a function that converts a string to a number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
-
 /**
  * _atoi - converts a string to an integer
  * @s : string argument
@@ -12,28 +11,25 @@ int _atoi(char *s)
 {
 	int i;
 	int len = strlen(s);
-	int num;
+	int num = 0;
 	int count = 0;
 	int digit;
+	int d;
 
 	for (i = 0; i < len; i++)
 	{
-		if(s[i] == '-')
-			digit++;
-		if(s[i] >= '0' && s[i] <= '9')
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - 48;
-			if (digit %2 == 0)
+			count++;
+			digit = s[i] - '0';
+			if (d % 2)
 				digit = -digit;
 			num = num * 10 + digit;
-                        count++;  
 		}
-		else
-	
-			continue;
 	}
 	if (count == 0)
 		return (0);
-	
 	return (num);
 }
